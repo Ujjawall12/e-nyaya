c7eb8989db037146bb14effe4fee8d322f713a0f
Ananya <ananyapratapsingh7@gmail.com>
2025-03-09 02:16:48 +0530

    Added Front-end using ViteJS Framework

---
+++ frontend-vite/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t border-gray-300 md:px-10 md:py-5 p-0 bottom-0 w-full">
+      <div className="flex xs:h-[6vh] md:h-auto items-center justify-center w-full">
+        <p className="text-blue-500 font-montserrat md:text-sm text-xs">
+            © Electrothon Project E-Nyaya 2025 | Ananya | Ujjawal | Pratyush | Archisman
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
